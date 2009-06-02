e48f28c8a84cfa967d5a6fd2498b596bb729a8ac
Ari Epstein <ate2@wings.assembly.cornell.edu>
2009-06-02 16:18:00 -0400

    Remove unneeded migration.

--- db/migrate/20090602153313_add_total_to_administrative_expenses.rb
+++
@@ -1,9 +0,0 @@
-class AddTotalToAdministrativeExpenses < ActiveRecord::Migration
-  def self.up
-    add_column :administrative_expenses, :total, :decimal
-  end
-
-  def self.down
-    remove_column :administrative_expenses, :total
-  end
-end
