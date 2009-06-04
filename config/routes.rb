c7238dfa066ca1e8f1c61ef617e0b60b71fa6c00
Lovish Agarwal <la254@oa-dev2.assembly.cornell.edu>
2009-06-04 09:58:11 -0400

    Commit prior change.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :publication_expenses
+
   map.resources :organizations
 
   # The priority is based upon order of creation: first created -> highest priority.
