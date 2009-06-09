62adf258c02769d261012fd2b362dd287bc1d619
Hari Sankar <hss78@wings.assembly.cornell.edu>
2009-06-09 15:20:35 -0400

    Comment out problematic lines.

--- app/models/request_item.rb
+++ app/models/request_item.rb
@@ -3,7 +3,7 @@ class RequestItem < ActiveRecord::Base
   belongs_to :request_node
   belongs_to :request
 
-  acts_as_tree
-  acts_as_list :scope => :parent_id
+  #acts_as_tree
+  #acts_as_list :scope => :parent_id
 end
 
