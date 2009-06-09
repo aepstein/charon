3682a29d57c75ef56b7d5f0445ea274562be23ec
Jesse Miner <jdm65@oa-dev1.dayadmin.cornell.edu>
2009-06-09 17:04:02 -0400

    beginning javascript

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,18 @@
 module RequestsHelper
+  def add_item_link(name)
+    link_to_function name do |page|
+      page.insert_html :bottom, :items, :partial => 'item_form', :object => nil
+    end
+  end
 end
+
+#  def add_task_link(name, form)
+  #  link_to_function name do |page|
+  #    task = render(:partial => 'node_form', :locals => { :pf => form })
+  #    page << %{
+#var new_task_id = "new_" + new Date().getTime();
+#$('tasks').insert({ bottom: "#{ escape_javascript task }".replace(/new_\\d+/g, new_task_id) });
+#}
+ #   end
+  #end
+
