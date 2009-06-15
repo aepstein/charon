fa929a2b3c199bafb93c0ea8cafbb73282b40993
Ari Epstein <ate2@oa-dev3.dayadmin.cornell.edu>
2009-06-15 09:16:02 -0400

    Tweak Jesse's js.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,7 @@
 class Request < ActiveRecord::Base
   has_many :request_items
+  acts_as_tree
   accepts_nested_attributes_for :request_items
+  accepts_nested_attributes_for :children
 end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,13 +14,13 @@ function add_child_item(el, depth) {
     var id_children_str = "";
     var name_children_str = "";
     for (i = 0; i < depth; i++) {
-      id_children_str += "_new_request_items_";
-      name_children_str += "[new_request_items][]";
+      id_children_str += "_request_items_";
+      name_children_str += "[children][]";
     }
 
     var html = '\n<label for="request_new_request_items__request_amount">Request amount</label>\n';
-    html += '<input id="request' + id_children_str + '_request_amount" name="request' + name_children_str + '[request_amount]" size="30" type="text">\n';
-    html += '<input id="request' + id_children_str + '_should_destroy" name="request' + name_children_str + '[should_destroy]" value="0" type="hidden" class="should_destroy">\n';
+    html += '<input id="request' + id_children_str + '_request_amount" name="request[request_items][]' + name_children_str + '[request_amount]" size="30" type="text">\n';
+    html += '<input id="request' + id_children_str + '_should_destroy" name="request[request_items][]' + name_children_str + '[should_destroy]" value="0" type="hidden" class="should_destroy">\n';
     html += '<a href="#" onclick="mark_to_destroy(this.up()); return false;">remove</a><br />\n';
     html += '<a href="#" onclick="add_child_item(this.up(), ' + (depth + 1) + '); return false;">add child item</a>\n\n';
 
