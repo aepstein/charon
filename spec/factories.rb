5aa3be39506263dc03f38afc7775d3226a2542de
Ari Epstein <ate2@oa-dev3.dayadmin.cornell.edu>
2009-06-26 09:18:07 -0400

    Clean out factories.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,1 @@
-Factory.define :request_structure do |request_structure|
-  Factory.sequence :request_structure_name do |sequence|
-    "request_structure##{sequence}"
-  end
-  request_structure.name Factory.next :request_structure_name
-end
-
-Factory.define :request_node do |request_node|
-  request_node.association :request_structure
-end
 
