c1220b3885368cbe89feedbdb7e6de83fcf1f2f5
Ari Epstein <ate2@wings.assembly.cornell.edu>
2009-08-20 16:46:21 -0400

    Initial capistrano deployment.

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+# deploy.rb
+set :application, "charon"
+role :app, "assembly.cornell.edu"
+role :web, "assembly.cornell.edu"
+role :db,  "assembly.cornell.edu", :primary => true
+
+set :user, "www-data"
+set :deploy_to, "/var/www/assembly/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git://assembly.cornell.edu/git/#{application}.git"
+set :branch, "master"
+set :git_enable_submodules, 0
+
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/db/uploads #{release_path}/db/uploads"
+    #run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
+
+  desc "Sync the public/assets directory."
+  task :assets do
+    #system "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
+
