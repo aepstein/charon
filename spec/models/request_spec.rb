d4dee1913432de9a37290c4868c2ec579ab1e773
Ari Epstein <ate2@yofi.arimahome.net>
2009-09-15 23:32:50 -0400

    Upgrade to latest rails version, eliminate defunct spec.

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -13,14 +13,6 @@ describe Request do
     @request.save.should == true
   end
 
-  it "should not create a new instance after a basis has closed" do
-    request = Factory.build(:request)
-    request.basis.open_at = DateTime.now - 2.days
-    request.basis.closed_at = DateTime.now - 1.days
-    request.basis.save.should == true
-    request.save.should == false
-  end
-
   it "should not save without an organization" do
     @request.organizations.delete_all
     @request.save.should == false
