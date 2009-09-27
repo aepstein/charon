202067685eea376cd68381530e9105f06611e3a6
Ari Epstein <ate2@yofi.arimahome.net>
2009-09-27 13:34:05 -0400

    Improve display slightly.

--- app/controllers/local_event_expenses_controller.rb
+++ app/controllers/local_event_expenses_controller.rb
@@ -3,7 +3,8 @@ class LocalEventExpensesController < ApplicationController
 
   def index
     @events = LocalEventExpense.find( :all, :include => { :version => { :item => { :request => { :organizations => :users } } } },
-      :conditions => ['versions.perspective = ? AND local_event_expenses.date >= ?', 'reviewer', Date.today] )
+      :conditions => ['versions.perspective = ? AND local_event_expenses.date >= ?', 'reviewer', Date.today],
+      :order => 'local_event_expenses.date ASC' )
     page = params[:page] ? params[:page] : 1
     respond_to do |format|
       format.html { @events = @events.paginate( :page => page ) }
