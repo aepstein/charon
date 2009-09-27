cdad1e4b97cb5ed45706fe2cc606b6ed4f33719d
Ari Epstein <ate2@yofi.arimahome.net>
2009-09-27 11:43:43 -0400

    Order categories by name

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,8 @@ class Category < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
+  default_scope :name, :order => "categories.name ASC"
+
   include GlobalModelAuthorization
 
   def to_s
