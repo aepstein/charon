f70e8b58578924f1c35c175c108bc1168b7915e7
Ari Epstein <ate2@yofi.arimahome.net>
2009-09-27 13:54:43 -0400

    Add csv

--- app/controllers/local_event_expenses_controller.rb
+++ app/controllers/local_event_expenses_controller.rb
@@ -8,6 +8,21 @@ class LocalEventExpensesController < ApplicationController
     page = params[:page] ? params[:page] : 1
     respond_to do |format|
       format.html { @events = @events.paginate( :page => page ) }
+      format.csv do
+        csv_string = FasterCSV.generate do |csv|
+          csv << %w( date title location uup purpose organizers contacts )
+          @events.each do |event|
+            csv << ( [ local_event.date,
+                       local_event.title,
+                       local_event.location,
+                       local_event.uup_required? ? 'Y' : 'N',
+                       local_event.purpose,
+                       local_event.requestors.map { |r| r.name }.join(", "),
+                       local_event.requestors.map { |r| r.users }.flatten.uniq.map { |u| "#{u} (#{u.net_id})" }.join(", ") ] )
+          end
+        end
+        send_data csv_string, :disposition => "attachment; filename=local_events.csv"
+      end
     end
   end
 end
