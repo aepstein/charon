4d33b6e555db9a0305e884f9c7a2fbe748a30393
Ari Epstein <ate2@yofi.arimahome.net>
2010-01-09 16:29:38 -0500

    Add some validations and spec for requirement model.

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -4,5 +4,7 @@ class Requirement < ActiveRecord::Base
 
   validates_presence_of :permission
   validates_presence_of :fulfillable
+  validates_uniqueness_of :permission_id, :scope => [ :fulfillable_id, :fulfillable_type ]
+  validates_inclusion_of :fulfillable_type, :in => Fulfillment::FULFILLABLE_TYPES.values.flatten
 end
 
--- spec/models/requirement_spec.rb
+++ spec/models/requirement_spec.rb
@@ -8,5 +8,28 @@ describe Requirement do
   it "should create a new instance given valid attributes" do
     @requirement.id.should_not be_nil
   end
+
+  it 'should not save without a permission' do
+    @requirement.permission = nil
+    @requirement.save.should be_false
+  end
+
+  it 'should not save without a fulfillable' do
+    @requirement.fulfillable = nil
+    @requirement.save.should be_false
+  end
+
+  it 'should not save a duplicate' do
+    duplicate = Factory.build(:requirement)
+    duplicate.permission = @requirement.permission
+    duplicate.fulfillable = @requirement.fulfillable
+    duplicate.save.should be_false
+  end
+
+  it 'should not save a fulfillable that is not of a valid type' do
+    @requirement.fulfillable = Factory(:user)
+    Fulfillment::FULFILLABLE_TYPES.values.flatten.should_not include @requirement.fulfillable_type
+    @requirement.save.should be_false
+  end
 end
 
