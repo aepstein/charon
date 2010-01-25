5f3a00815c83be764d525e00c67d760d16b5ed20
Ari Epstein <aepstein607@gmail.com>
2010-01-25 15:03:29 -0500

    Default is now to log in the user directly.

--- config/routes.rb
+++ config/routes.rb
@@ -84,7 +84,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  map.root :controller => "users", :action => "profile" # optional, this just sets the root route
 
 end
 
