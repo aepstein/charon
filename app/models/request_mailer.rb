324b998c2e548b7d100c095dce1463cce6feca23
Ari Epstein <ate2@yofi.arimahome.net>
2010-04-25 23:02:32 -0400

    Start implementation of reminders for requestors.

--- app/models/request_mailer.rb
+++ app/models/request_mailer.rb
@@ -1,5 +1,19 @@
 class RequestMailer < ActionMailer::Base
+  def started_reminder(request)
+    recipients  request.requestors.may('update').map { |u| "#{u.name} <#{u.email}>" }
+    from        "#{request.contact_name} <#{request.contact_email}>"
+    subject     "Your #{request} needs attention"
+    sent_on     Time.now
+    body        :request => request
+  end
 
+  def completed_reminder(request)
+    recipients  request.requestors.may('approve').map { |u| "#{u.name} <#{u.email}>" }
+    from        "#{request.contact_name} <#{request.contact_email}>"
+    subject     "Your #{request} needs approval"
+    sent_on     Time.now
+    body        :request => request
+  end
 
   def release_notice(request)
     recipients  request.requestors.may('review').map { |u| "#{u.name} <#{u.email}>" }
