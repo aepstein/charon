1a51437fd99788710b7f4af87633983d19e0e901
Ari Epstein <aepstein607@gmail.com>
2010-06-03 10:19:19 -0400

    Improve authorization rules.

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -42,8 +42,47 @@ authorization do
       if_permitted_to :request
       if_attribute :status => is_in { %w( started ) }
     end
-    has_permission_on [ :requests ], :to => [ :show, :index ], :join_by => :and do
+    has_permission_on [ :requests ], :to => :approve, :join_by => :and do
       if_permitted_to :request
+      if_attribute :status => is_in { %w( started completed ) }
+    end
+    has_permission_on [ :requests ], :to => :approve, :join_by => :and do
+      if_permitted_to :review
+      if_attribute :status => is_in { %w( accepted reviewed ) }
+    end
+    has_permission_on [ :items ], :to => :request do
+      if_permitted_to :request, :request
+    end
+    has_permission_on [ :items ], :to => :manage do
+      if_permitted_to :manage, :request
+    end
+    has_permission_on [ :items ], :to => :review do
+      if_permitted_to :review, :request
+    end
+    has_permission_on [ :editions ], :to => :manage, :join_by => :and do
+      if_permitted_to :manage, :item
+    end
+    has_permission_on [ :editions ], :to => :request, :join_by => :and do
+      if_permitted_to :request, :item
+      if_attribute :perspective => is { Edition::PERSPECTIVES.first }
+    end
+    has_permission_on [ :editions ], :to => :review, :join_by => :and do
+      if_permitted_to :review, :item
+    end
+    has_permission_on [ :editions ], :to => :manage, :join_by => :and do
+      if_permitted_to :review, :item
+      if_attribute :item => { :request => { :status => is { 'accepted' } } }
+      if_attribute :perspective => is { Edition::PERSPECTIVES.last }
+    end
+    has_permission_on [ :approvals ], :to => [ :new, :create ] do
+      if_permitted_to :approve, :approvable
+    end
+    has_permission_on [ :approvals ], :to => [ :destroy ], :join_by => :and do
+      if_permitted_to :unapprove, :approvable
+      if_attribute :user_id => is { user.id }
+    end
+    has_permission_on [ :approvals ], :to => [ :destroy ] do
+      if_permitted_to :manage, :approvable
     end
   end
   role :guest do
@@ -52,6 +91,15 @@ authorization do
 end
 
 privileges do
+  privelege :request do
+    includes :show, :index
+  end
+  privilege :review do
+    includes :show, :index
+  end
+  privelege :approve do
+    includes :show
+  end
   privilege :manage do
     includes :create, :new, :update, :edit, :destroy, :show, :index
   end
