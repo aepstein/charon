20b642f0266707a69efb59008e30e6c484d4f774
Ari Epstein <ate2@yofi.arimahome.net>
2010-06-08 08:26:11 -0400

    Perfect tests for approvers controller.

--- app/models/approver.rb
+++ app/models/approver.rb
@@ -11,15 +11,5 @@ class Approver < ActiveRecord::Base
   default_scope :include => [:role], :order => 'status ASC, perspective ASC, roles.name ASC'
   named_scope :status, lambda { |status| { :conditions => { :status => status } } }
 
-  delegate :may_update?, :to => :framework
-  delegate :may_see?, :to => :framework
-
-  def may_create?(user)
-    may_update? user
-  end
-
-  def may_destroy?(user)
-    may_update? user
-  end
 end
 
