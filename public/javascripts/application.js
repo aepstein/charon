36adc3fb43e7c17895563a6c083e1beff586bb15
Ari Epstein <ate2@cornell.edu>
2011-03-21 10:48:05 -0400

    Tweak timepicker configuration.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-  $('input.ui-date-picker').datepicker({ dateFormat: 'yyyy-mm-dd' });
-  $('input.ui-datetime-picker').datetimepicker({ dateFormat: 'yyyy-mm-dd', timeFormat: 'hh:mm T'  });
+  $('input.ui-date-picker').datepicker({ dateFormat: 'yy-mm-dd' });
+  $('input.ui-datetime-picker').datetimepicker({ dateFormat: 'yy-mm-dd', timeFormat: 'hh:mm T'  });
 });
 
