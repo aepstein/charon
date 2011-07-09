e2ddc72c3fd1831e9af8df738a78a4806b3045f6
Ari Epstein <aepstein607@gmail.com>
2011-07-09 07:27:20 -0400

    Fix previous_perspective method

--- app/models/fund_edition.rb
+++ app/models/fund_edition.rb
@@ -120,7 +120,8 @@ class FundEdition < ActiveRecord::Base
   # Returns the perspective of previous edition in sequence
   # * returns nil if this is initial edition
   def previous_perspective
-    return nil unless i = perspective_index && i && i > 0
+    i = perspective_index
+    return nil unless i && i > 0
     PERSPECTIVES[i - 1]
   end
 
