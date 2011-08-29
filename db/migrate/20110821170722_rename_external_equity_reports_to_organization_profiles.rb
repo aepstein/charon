e19be4de0e11e82abbb09ed7f9559daf0d14f7b4
Ari Epstein <aepstein607@gmail.com>
2011-08-29 07:58:02 -0400

    Fix organization profile migration code.

--- db/migrate/20110821170722_rename_external_equity_reports_to_organization_profiles.rb
+++ db/migrate/20110821170722_rename_external_equity_reports_to_organization_profiles.rb
@@ -24,21 +24,31 @@ class RenameExternalEquityReportsToOrganizationProfiles < ActiveRecord::Migratio
   end
 
   def up
-    remove_index :external_equity_reports, :fund_edition_id
-    rename_table :external_equity_reports, :organization_profiles
-    add_column :organization_profiles, :organization_id, :integer
+    create_table :organization_profiles do |t|
+      t.integer  "organization_id", :null => false
+      t.decimal  "anticipated_expenses", :precision => 13, :scale => 2, :default => 0.0, :null => false
+      t.decimal  "anticipated_income", :precision => 13, :scale => 2, :default => 0.0, :null => false
+      t.decimal  "current_assets", :precision => 13, :scale => 2, :default => 0.0, :null => false
+      t.decimal  "current_liabilities", :precision => 13, :scale => 2, :default => 0.0, :null => false
+      t.boolean  "academic_credit"
+      t.timestamps
+    end
+    add_index :organization_profiles, :organization_id, :unique => true
     say_with_time 'Associating profiles with organizations and culling old versions' do
       OrganizationProfile.reset_column_information
-      OrganizationProfile.update_all( 'organization_id = ' +
-        '( SELECT organization_id FROM fund_grants INNER JOIN fund_requests ON ' +
-        '  fund_grants.id = fund_requests.fund_grant_id INNER JOIN fund_editions ' +
-        '  ON fund_requests.id = fund_editions.fund_request_id ' +
-        '  WHERE fund_editions.id = organization_profiles.fund_edition_id ) ' )
-      change_column :organization_profiles, :organization_id, :integer, :null => false
-      OrganizationProfile.reset_column_information
-      OrganizationProfile.group('organization_id').having('updated_at < MAX(updated_at)').delete_all
-      add_index :organization_profiles, :organization_id, :unique => true
-      remove_column :organization_profiles, :fund_edition_id
+      OrganizationProfile.connection.execute <<-SQL
+        INSERT INTO organization_profiles ( anticipated_expenses,
+        anticipated_income, current_assets, current_liabilities, academic_credit,
+        organization_id, created_at, updated_at )
+        SELECT anticipated_expenses, anticipated_income, current_assets,
+        current_liabilities, academic_credit, organization_id,
+        fund_editions.created_at, fund_editions.updated_at
+        FROM fund_grants INNER JOIN fund_requests ON fund_grants.id = fund_requests.fund_grant_id
+        INNER JOIN fund_editions ON fund_requests.id = fund_editions.fund_request_id
+        INNER JOIN external_equity_reports ON fund_editions.id = external_equity_reports.fund_edition_id
+        GROUP BY fund_grants.organization_id
+        ORDER BY fund_editions.created_at DESC
+      SQL
     end
     say_with_time 'Removing nodes and items associated with external equity reports' do
       FundItem.joins(:node).where( 'nodes.requestable_type = ?', 'ExternalEquityReport' ).each do |item|
@@ -49,6 +59,8 @@ class RenameExternalEquityReportsToOrganizationProfiles < ActiveRecord::Migratio
         node.destroy
       end
     end
+    remove_index :external_equity_reports, :fund_edition_id
+    drop_table :external_equity_reports
   end
 
   def down
