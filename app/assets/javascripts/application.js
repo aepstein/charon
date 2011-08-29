73fbe7655a9a68dd6e7e52b2c0bac1653ef7217f
Ari Epstein <ate2@cornell.edu>
2011-08-29 09:34:43 -0400

    Upgrade jquery timepicker.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,11 @@
 //= require jquery_ujs
 //= require_tree .
 
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  $('input.ui-date-picker').datepicker({ dateFormat: 'yy-mm-dd' });
+  $('input.ui-datetime-picker').datetimepicker({ ampm: true,
+    dateFormat: 'yy-mm-dd', timeFormat: 'hh:mm tt'  });
+});
+
