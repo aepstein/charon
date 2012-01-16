2fb057f9aaa82a6dd7a1b99e682e9ef332e3301a
Ari Epstein <aepstein607@gmail.com>
2012-01-16 07:13:51 -0500

    Add elements to modify/review member sources through organization edit functionality.

--- app/models/member_source.rb
+++ app/models/member_source.rb
@@ -1,15 +1,15 @@
 class MemberSource < ActiveRecord::Base
-  belongs_to :organization, :inverse_of => :member_sources
-  belongs_to :role, :inverse_of => :member_sources
-  has_many :memberships, :dependent => :destroy, :inverse_of => :member_source
-  has_many :users, :through => :memberships
+  belongs_to :organization, inverse_of: :member_sources
+  belongs_to :role, inverse_of: :member_sources
+  has_many :memberships, dependent: :destroy, inverse_of: :member_source
+  has_many :users, through: :memberships
 
-  validates :organization, :presence => true
-  validates :role, :presence => true
+  validates :organization, presence: true
+  validates :role, presence: true
   validates :minimum_votes,
-    :numericality => { :integer_only => true, :greater_than_or_equal_to => 0 }
+    numericality: { integer_only: true, greater_than_or_equal_to: 0 }
   validates :external_committee_id,
-    :numericality => { :integer_only => true, :greater_than => 0 }
+    numericality: { integer_only: true, greater_than: 0 }
 
 end
 
