783ff1bb89004cbacacfc61d460466b8dadb8ddb
Ari Epstein <ate2@cornell.edu>
2012-01-26 11:19:51 -0500

    Remove force_ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,6 @@ Charon::Application.configure do
   config.serve_static_assets = false
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.force_ssl = true
   config.assets.js_compressor  = :uglifier
   config.assets.css_compressor = :scss
   config.assets.compress = true
