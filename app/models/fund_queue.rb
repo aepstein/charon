26335d33f93535705046fb08b609a2fbbf3e9800
Ari Epstein <aepstein607@gmail.com>
2012-02-10 07:32:27 -0500

    Report review states for fund_queues.

--- app/models/fund_queue.rb
+++ app/models/fund_queue.rb
@@ -48,6 +48,30 @@ class FundQueue < ActiveRecord::Base
         rows.each { |row| csv << row }
       end
     end
+
+    # Returns an array with rows representing different request review states.
+    # For each row, contents are:
+    # * review state name
+    # * number of distinct requests in review state
+    # * total requestor amounts for review state
+    # * total reviewer amounts for review state
+    def quantity_and_amount_report
+      connection.select_rows(
+        "SELECT fund_requests.review_state AS review_state, " +
+        "COUNT(DISTINCT fund_requests.id) AS quantity, " +
+        "SUM(requests.amount) AS request, " +
+        "SUM(reviews.amount) AS review " +
+        "FROM fund_grants INNER JOIN fund_requests ON fund_grants.id = fund_requests.fund_grant_id " +
+        "LEFT JOIN fund_editions AS requests ON fund_requests.id = requests.fund_request_id " +
+        "AND requests.perspective = #{connection.quote FundEdition::PERSPECTIVES.first} " +
+        "LEFT JOIN fund_editions AS reviews ON requests.fund_request_id = reviews.fund_request_id " +
+        "AND reviews.perspective = #{connection.quote FundEdition::PERSPECTIVES.last} " +
+        "AND reviews.fund_item_id = requests.fund_item_id " +
+        "WHERE fund_requests.fund_queue_id = #{proxy_association.owner.id} " +
+        "GROUP BY fund_requests.review_state ORDER BY fund_requests.review_state"
+      )
+    end
+
   end
   has_many :fund_editions, :through => :fund_requests, :uniq => true
   has_many :fund_items, :through => :fund_requests, :uniq => true do
