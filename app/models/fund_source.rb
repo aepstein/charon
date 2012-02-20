5e8bb6d8085d7f1c851dcc6372c939a10add701c
Ari Epstein <ate2@cornell.edu>
2012-02-20 11:03:38 -0500

    Fix bulk query.

--- app/models/fund_source.rb
+++ app/models/fund_source.rb
@@ -26,7 +26,8 @@ class FundSource < ActiveRecord::Base
         AS name, EXISTS(SELECT * FROM fund_requests AS r INNER JOIN fund_grants
         AS g ON r.fund_grant_id = g.id INNER JOIN returning_fund_sources AS rs
         ON g.fund_source_id = rs.returning_fund_source_id WHERE
-        rs.fund_source_id = fund_grants.fund_source_id AND r.state = 'released')
+        rs.fund_source_id = fund_grants.fund_source_id AND r.state = 'released'
+        AND g.organization_id = fund_grants.organization_id)
         AS returning, (SELECT registered FROM registrations INNER JOIN
         registration_terms ON registrations.registration_term_id =
         registration_terms.id WHERE registration_terms.current = 1 AND
