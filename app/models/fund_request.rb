fa031bd01026ae70b923e9661b1571679ee11050
Ari Epstein <aepstein607@gmail.com>
2012-02-27 18:57:18 -0500

    Add flag to show whether organization is considered returning for the purposes of the request.

--- app/models/fund_request.rb
+++ app/models/fund_request.rb
@@ -299,7 +299,7 @@ class FundRequest < ActiveRecord::Base
     end
   end
 
-  delegate :require_requestor_recipients!, to: :fund_grant
+  delegate :require_requestor_recipients!, :returning?, to: :fund_grant
 
   # Is the request actionable?
   def actionable?; UNACTIONABLE_STATES.map(&:to_s).all? { |s| s != state }; end
