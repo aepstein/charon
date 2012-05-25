9244f7f1e970602b7dc089de767d6ea27e5e96a6
Ari Epstein <ate2@cornell.edu>
2012-05-25 16:30:20 -0400

    Some work on final vs. pending fund_allocations.

--- app/models/fund_allocation.rb
+++ app/models/fund_allocation.rb
@@ -9,5 +9,10 @@ class FundAllocation < ActiveRecord::Base
     numericality: { greater_than_or_equal_to: 0.0 }
   validates :fund_item, presence: true
   validates :fund_request, presence: true
+
+  scope :final, lambda { where { fund_request_id.in(
+    FundRequest.unscoped.with_state(:allocated).select { id } ) } }
+  scope :pending, lambda { where { fund_request_id.not_in(
+    FundRequest.unscoped.with_state(:allocated).select { id } ) } }
 end
 
