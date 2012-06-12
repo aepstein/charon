22bd59beeab98f9bed998308901740d68d7fbaa0
Ari Epstein <aepstein607@gmail.com>
2012-06-12 07:20:43 -0400

    Set up initial framework fulfillment information for memberships.

--- db/migrate/20120611153946_create_frameworks_memberships.rb
+++ db/migrate/20120611153946_create_frameworks_memberships.rb
@@ -1,31 +1,27 @@
 class CreateFrameworksMemberships < ActiveRecord::Migration
-  class Framework < ActiveRecord::Base
-  end
-
   def up
-#    create_table :frameworks_memberships, id: false do |t|
-#      t.column :framework_id, :integer, null: false
-#      t.column :membership_id, :integer, null: false
-#    end
-#    add_index :frameworks_memberships, [ :framework_id, :membership_id ],
-#      unique: true
-    Framework.each do |framework|
-      say 'Converting existing fulfillments in to framework fulfillments for memberships...'
-      execute <<-SQL
-        INSERT INTO frameworks_memberships ( framework_id, membership_id )
-        SELECT #{framework.id}, memberships.id FROM memberships
-        LEFT JOIN requirements ON requirements.framework_id = #{framework.id} AND
-        ( requirements.role_id IS NULL OR requirements.role_id = memberships.role_id )
-        LEFT JOIN fulfillments ON fulfillments.fulfillable_type = requirements.fulfillable_type AND
-        fulfillments.fulfillable_id = requirements.fulfillable_id AND
-        ( ( fulfiller_type = 'User' AND fulfiller_id = memberships.user_id ) OR
-            ( fulfiller_type = 'Organization' AND fulfiller_id = memberships.organization_id ) )
-        WHERE memberships.active = 1 AND ( requirements.id IS NULL OR
-          fulfillments.id IS NOT NULL )
-        GROUP BY memberships.id
-        ORDER BY fulfillments.id ASC
-      SQL
+    create_table :frameworks_memberships, id: false do |t|
+      t.column :framework_id, :integer, null: false
+      t.column :membership_id, :integer, null: false
     end
+    add_index :frameworks_memberships, [ :framework_id, :membership_id ],
+      unique: true
+    say 'Converting existing fulfillments in to framework fulfillments for memberships... (this may take a while)'
+    execute <<-SQL
+      INSERT INTO frameworks_memberships ( framework_id, membership_id )
+      SELECT frameworks.id, memberships.id FROM frameworks, memberships
+      WHERE memberships.active = 1 AND
+      (SELECT COUNT(*) FROM requirements WHERE framework_id = frameworks.id AND
+      (requirements.role_id IS NULL OR requirements.role_id = memberships.role_id))
+      =
+      (SELECT COUNT(*) FROM requirements INNER JOIN fulfillments
+      WHERE framework_id = frameworks.id AND requirements.fulfillable_type =
+      fulfillments.fulfillable_type AND requirements.fulfillable_id =
+      fulfillments.fulfillable_id AND
+      (( fulfiller_type = 'User' AND fulfiller_id = memberships.user_id ) OR
+      ( fulfiller_type = 'Organization' AND fulfiller_id = memberships.organization_id )) AND
+      (requirements.role_id IS NULL OR requirements.role_id = memberships.role_id))
+    SQL
   end
 
   def down
