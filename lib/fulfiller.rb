818468336bb11216535164301b3b1953a6c3639b
Ari Epstein <ate2@cornell.edu>
2012-06-13 15:15:28 -0400

    Remove more fulfillment code.

--- lib/fulfiller.rb
+++ lib/fulfiller.rb
@@ -9,26 +9,6 @@ module Fulfiller
         self.fulfillable_types = []
       end
 
-      fulfillable_types.each do |fulfillable_type|
-        class_eval <<-RUBY
-          has_many :fulfillable_#{fulfillable_type.underscore.pluralize},
-          through: :fulfillments, source: :fulfillable,
-          source_type: "#{fulfillable_type}" do
-            def qualifying
-              #{fulfillable_type}.unscoped.fulfilled_by proxy_association.owner
-            end
-
-            def addable
-              qualifying.where { |f| f.id.not_in( scoped.select { id } ) }
-            end
-
-            def deletable
-              scoped.where { |f| f.id.not_in( qualifying.select { id } ) }
-            end
-          end
-        RUBY
-      end
-
       self.fulfillable_types += fulfillable_types
 
       send :include, InstanceMethods
