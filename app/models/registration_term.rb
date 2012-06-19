a4285fc5abfd5c487eae5533799ffddcc9090271
Ari Epstein <aepstein607@gmail.com>
2012-06-18 22:57:49 -0400

    All specs pass.

--- app/models/registration_term.rb
+++ app/models/registration_term.rb
@@ -4,6 +4,9 @@ class RegistrationTerm < ActiveRecord::Base
 
   scope :current, where( current: true )
 
+  validates :external_id, numericality: { integer_only: true, greater_than: 0 },
+    presence: true
+
   has_many :registrations, dependent: :nullify, inverse_of: :registration_term do
     # Update registrations:
     # * dissociate registrations that do not have a non-matching external term id
@@ -42,6 +45,7 @@ class RegistrationTerm < ActiveRecord::Base
   # on time attributes
   def changed_significantly?
     return false unless changed?
+    return true if ( changes.keys - %w( starts_at ends_at ) ).any?
     %w( starts_at ends_at ).each do |attribute|
       if send "#{attribute}_changed?"
         return true if changes[attribute].first.to_i != changes[attribute].last.to_i
@@ -58,9 +62,10 @@ class RegistrationTerm < ActiveRecord::Base
   def update_dependencies
     return true unless current_changed?
     memberships.activate!
-    Framework.where { id.in( Requirement.where { must_register.eq( true ) }.
+    Framework.where { id.in( Requirement.joins { fulfillable(RegistrationCriterion) }.
+      where { registration_criterions.must_register.eq( true ) }.
       select { framework_id } ) }.each do |framework|
-      framework.memberships.update!
+      framework.update_memberships
     end
     true
   end
